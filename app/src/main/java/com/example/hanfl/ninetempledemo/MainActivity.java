710965ec990009035070bbfc9f1b9dc2a723f2e1
huyas <yokozen@126.com>
2018-01-22 12:58:41 +0800

    使签到可以展示。

--- app/src/main/java/com/example/hanfl/ninetempledemo/MainActivity.java
+++ app/src/main/java/com/example/hanfl/ninetempledemo/MainActivity.java
@@ -1,39 +1,15 @@
 package com.example.hanfl.ninetempledemo;
 
 import android.app.Activity;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-import com.example.hanfl.ninetempledemo.adapter.DemoAdapter;
-import com.example.hanfl.ninetempledemo.data.PrizeData;
 import com.example.hanfl.ninetempledemo.eventbus.MessageEvent;
-import com.example.hanfl.ninetempledemo.utils.AppJsonFileReader;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
-import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.lang.ref.WeakReference;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.UUID;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -63,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
     }
     @OnClick(R.id.btn_sign)
     void onSign(View view) {
-        Toast.makeText(mActivity, "进入签到", Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(this, SignCalenderActivity.class));
     }
 
     @Override
---
+++ app/src/main/java/com/example/hanfl/ninetempledemo/SignCalenderActivity.java
@@ -0,0 +1,93 @@
+package com.example.hanfl.ninetempledemo;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.test.sign_calender.DPCManager;
+import com.test.sign_calender.DPDecor;
+import com.test.sign_calender.DatePicker;
+import com.test.sign_calender.DatePicker2;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Iterator;
+import java.util.List;
+
+import butterknife.ButterKnife;
+
+public class SignCalenderActivity extends AppCompatActivity {
+
+    private Activity mActivity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_calender);
+        mActivity = this;
+        ButterKnife.bind(this);
+        myCalendar();
+    }
+
+    private void myCalendar() {
+//        gson = new Gson();
+
+//        List<String> tmp = gson.fromJson(SignActivity.sign_time, new TypeToken<List<String>>() {
+//        }.getType());
+        List<String> tmp = new ArrayList<>();
+        tmp.add("2016-8-2");
+        tmp.add("2016-8-4");
+        tmp.add("2016-08-21");
+        tmp.add("2016-08-09");
+        DPCManager.getInstance().setDecorBG(tmp);
+
+        DatePicker2 picker = (DatePicker2) findViewById(R.id.dp_sign_calender);
+
+        picker.setFestivalDisplay(true); //是否显示节日
+        picker.setHolidayDisplay(true); //是否显示假期
+        picker.setDeferredDisplay(true); //是否显示补休
+
+        Calendar c = Calendar.getInstance();//首先要获取日历对象
+        int mYear = c.get(Calendar.YEAR); // 获取当前年份
+        final int mMonth = c.get(Calendar.MONTH) + 1;// 获取当前月份
+        picker.setDate(mYear, mMonth);
+
+        picker.setDPDecor(new DPDecor() {
+            @Override
+            public void drawDecorBG(Canvas canvas, Rect rect, Paint paint) {
+                paint.setColor(Color.RED);
+                //                paint.setStyle(Paint.Style.STROKE);
+                //                paint.setTextAlign(Paint.Align.CENTER);
+                //                paint.setTextSize(16);
+                paint.setAntiAlias(true);
+                @SuppressWarnings("ResourceType")// huweiyang新加的
+                InputStream is = getResources().openRawResource(R.mipmap.ic_launcher);
+                Bitmap mBitmap = BitmapFactory.decodeStream(is);
+                canvas.drawBitmap(mBitmap, rect.centerX() - mBitmap.getWidth() / 2f, rect.centerY() - mBitmap.getHeight() / 2f, paint);
+            }
+        });
+        picker.setOnDateSelectedListener(new DatePicker.OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(List<String> date) {
+                String result = "";
+                Iterator iterator = date.iterator();
+                while (iterator.hasNext()) {
+                    result += iterator.next();
+                    if (iterator.hasNext()) {
+                        result += "\n";
+                    }
+                }
+                Toast.makeText(SignCalenderActivity.this, result, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+}
